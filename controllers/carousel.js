1300804a2acd114f6719762c548af2f90c558254
涛 <tao@taodeMacBook-Pro.local>
2020-02-04 15:04:10 +0800

    修改变量名称

--- controllers/carousel.js
+++ controllers/carousel.js
@@ -1,14 +1,10 @@
 const schema = require('async-validator').default;
 const Carousel = require('./../models/carousel.js')
-const {formatTime} = require('./../utils/authcode')
 const carouselController = {
   // 轮播图列表
   index: async function(req, res, next) {
     try {
       const carousels = await Carousel.all();
-      carousels.forEach(data => {
-        data.created_at = formatTime(data.created_at)
-      })
       res.json({error_code: 0, data: { carousels } })
     } catch (e) {
       res.json({error_code: 1, message: e.message})
--- controllers/qiniu.js
+++ controllers/qiniu.js
@@ -1,21 +1,21 @@
-var qiniu = require('qiniu');
-var accessKey = process.env.AccessKey;
-var secretKey = process.env.SecretKey
-var domain = process.env.domain
-var bucket = process.env.bucket
+const qiniu = require('qiniu');
+const accessKey = process.env.qiniuAccessKey;
+const secretKey = process.env.qiniuSecretKey
+const domain = process.env.qiniuDomain
+const bucket = process.env.qiniuBucket
 
 const qiniuController = {
   getQiniuToken:function(req, res,next){
     try{
-      var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-      var options = {
+      const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+      const options = {
           scope: bucket
       };
-      var putPolicy = new qiniu.rs.PutPolicy(options);
-      var uploadToken=putPolicy.uploadToken(mac);
-      res.json({code:0, data:{uploadToken,domain}, message:'请求成功'})
+      const putPolicy = new qiniu.rs.PutPolicy(options);
+      const token=putPolicy.uploadToken(mac);
+      res.json({error_code:0, data:{token,domain}, message:'请求成功'})
     }catch{
-      res.json({code:1, message:"服务器错误"})
+      res.json({error_code:1, message:"服务器错误"})
     }
   }
 };
